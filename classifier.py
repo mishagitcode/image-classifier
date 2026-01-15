25e090524dfab3a2653505d44ba48b4307e74b34
Misha Zubritsky <zubritsky.misha@gmail.com>
2026-01-15 18:13:13 +0200

    Refactor classifier.py

--- classifier.py
+++ classifier.py
@@ -1,33 +1,23 @@
 import tensorflow as tf
-
-
-IMAGE_SIZE = (100, 100)
-THRESHOLD = 0.5
-
-
-def preprocess_image(image):
-    img_array = tf.keras.preprocessing.image.img_to_array(image)
-    img_array = img_array / 255.0
-    img_array = tf.expand_dims(img_array, 0)
-
-    return img_array
-
-
-def load_and_preprocess_image(path):
-    image = tf.keras.preprocessing.image.load_img(
-        path, target_size=IMAGE_SIZE
-    )
-
-    return preprocess_image(image=image)
+import numpy as np
 
 
 def classify(model, image_path):
-    preprecessed_image = load_and_preprocess_image(path=image_path)
+    img = tf.keras.utils.load_img(image_path, target_size=(100, 100))
+    img_array = tf.keras.utils.img_to_array(img)
 
-    predictions = model.predict(preprecessed_image)
-    score = predictions[0][0]
+    img_array = img_array / 255.0
 
-    label = "apple" if score <= THRESHOLD else "pear"
-    prob = 1 - score if label == "apple" else score
+    img_array = np.expand_dims(img_array, axis=0)
+
+    predictions = model.predict(img_array)
+    score = float(predictions[0][0])
+
+    if score <= 0.5:
+        label = "Apple"
+        prob = (1 - score)
+    else:
+        label = "Pear"
+        prob = score
 
     return label, prob
